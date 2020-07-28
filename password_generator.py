5ec865d0742ff9a26b0b2caa2dd741c69ecd6a13
Anish Bhandarkar <68901351+AnishBhandarkar@users.noreply.github.com>
2020-07-28 18:32:31 +0530

    while loop added
    Using while loop we can get chance to generate password more than one time

--- password_generator.py
+++ password_generator.py
@@ -1,12 +1,14 @@
-# Password generator using Python
 import string
 import random
 
 if __name__ == "__main__":
-    s1 = string.ascii_lowercase
-    s2 = string.ascii_uppercase
-    s3 = string.digits
+    s1 = string.ascii_lowercase 
+    s2 = string.ascii_uppercase 
+    s3 = string.digits 
     s4 = string.punctuation
+    
+    i = 0
+while i <=2:  # Here we can get to chance to get password if we are not satisfied with first one
     pass_len = int(input("Enter password length : \n"))
     s = []
     s.extend(list(s1))
@@ -19,6 +21,5 @@ if __name__ == "__main__":
     print("Your password is :")
     print("".join(s[0:pass_len]))
     print("Thank you !")
-
-
-
+    i += 1
+		
